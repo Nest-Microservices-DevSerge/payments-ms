9c57ca6e2d140ad1b8da446769e9a0d07f824a7a
sergeavila7 <serge_avila7@yahoo.com>
2025-07-14 17:43:17 -0600

    feat(core): add payments config

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const logger = new Logger('Payments-ms');
 
   // const app = await NestFactory.createMicroservice<MicroserviceOptions>(
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { rawBody: true });
   //   AppModule,
   //   {
   //     transport: Transport.TCP,
@@ -18,12 +18,12 @@ async function bootstrap() {
   //   },
   // );
 
-  // app.useGlobalPipes(
-  //   new ValidationPipe({
-  //     whitelist: true,
-  //     forbidNonWhitelisted: true,
-  //   }),
-  // );
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
 
   await app.listen(3003);
 
