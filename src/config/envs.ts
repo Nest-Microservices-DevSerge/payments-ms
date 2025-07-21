a12d5d3e0ebdc9d3c670d0d6f7ebbb9b36534f73
sergeavila7 <serge_avila7@yahoo.com>
2025-07-21 17:08:15 -0600

    feat(core): migrate core module to microservice architecture

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -4,6 +4,7 @@ import * as joi from 'joi';
 
 interface EnvVars {
   PORT: number;
+  NATS_SERVERS: string[];
   STRIPE_SECRET: string;
   STRIPE_SUCCESS_URL: string;
   STRIPE_CANCEL_URL: string;
@@ -13,6 +14,7 @@ interface EnvVars {
 const envsSchema = joi
   .object({
     PORT: joi.number().required(),
+    NATS_SERVERS: joi.array().items(joi.string()).required(),
     STRIPE_SECRET: joi.string().required(),
     STRIPE_SUCCESS_URL: joi.string().required(),
     STRIPE_CANCEL_URL: joi.string().required(),
@@ -20,7 +22,10 @@ const envsSchema = joi
   })
   .unknown(true);
 
-const { error, value } = envsSchema.validate(process.env);
+const { error, value } = envsSchema.validate({
+  ...process.env,
+  NATS_SERVERS: process.env.NATS_SERVERS?.split(','),
+});
 
 if (error) {
   throw new Error(`Config validation error: ${error.message}`);
@@ -30,6 +35,7 @@ const envVars: EnvVars = value;
 
 export const envs = {
   port: envVars.PORT,
+  natsServers: envVars.NATS_SERVERS,
   stripeSecret: envVars.STRIPE_SECRET,
   stripeSuccessUrl: envVars.STRIPE_SUCCESS_URL,
   stripeCancelUrl: envVars.STRIPE_CANCEL_URL,
--- src/main.ts
+++ src/main.ts
@@ -2,21 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { envs } from './config';
-// import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const logger = new Logger('Payments-ms');
 
-  // const app = await NestFactory.createMicroservice<MicroserviceOptions>(
   const app = await NestFactory.create(AppModule, { rawBody: true });
-  //   AppModule,
-  //   {
-  //     transport: Transport.TCP,
-  //     options: {
-  //       port: envs.port,
-  //     },
-  //   },
-  // );
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -25,7 +16,16 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(3003);
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.NATS,
+    options: {
+      servers: envs.natsServers,
+    },
+  });
+
+  await app.startAllMicroservices();
+
+  await app.listen(envs.port);
 
   logger.log(`Payments Microservice running on port ${envs.port}`);
 }
